6af4e1907eb3ba5308471837952c56e22f26481f
Antho <anthony@happster.fr>
2014-01-30 15:10:03 +0100

    Initial Commit

---
+++ MyWeather/MWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MWAppDelegate.h
+//  MyWeather
+//
+//  Created by administrator on 30/01/14.
+//  Copyright (c) 2014 Happster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyWeather/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyWeather
+//
+//  Created by administrator on 30/01/14.
+//  Copyright (c) 2014 Happster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MWAppDelegate class]));
+    }
+}
